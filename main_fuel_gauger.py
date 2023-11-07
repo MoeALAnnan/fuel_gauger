1b0ed2f15473a6d26126b4b62f67cb5e0171742a
MoeALAnnan <113625144+MoeALAnnan@users.noreply.github.com>
2023-11-07 21:57:52 +0100

    Update main_fuel_gauger.py

--- main_fuel_gauger.py
+++ main_fuel_gauger.py
@@ -1,228 +1,163 @@
 #!/usr/bin/python3
 import tkinter
-import classes_page2
+from tkinter import ttk
+from PIL import Image, ImageTk
+from tkinter import StringVar, IntVar
+import classes
+from page1 import create_new_frame, go_to_next_page, configure_text_kilo_or_livre
+import frame_manager
+from tkcalendar import DateEntry
+from datetime import date
 import pickle
 from write import main
+window = tkinter.Tk()
+window.title("AIRBUS")
 
-checker_page2 = None
-def configure_text_kilo_or_livre_page2(check):
-        global checker_page2
-        checker_page2 = check
-        print(checker_page2)
-        if checker_page2 == 2:
-             uplifted_fuel_parameters_frame.config(text = "Uplifted Fuel Parameters - lb")
-        else:
-            pass
-def go_to_second_page():
-    second_frame.grid(row=0, column=0, sticky="news")
+ki_lo_var = IntVar()
+frame = tkinter.Frame(window)
+frame.pack()
 
-def create_2nd_frame(frame, button, continue_button):
-    next_page_button = button
-    next_page_button.configure(state=tkinter.NORMAL)
-    continue_button.configure(state=tkinter.DISABLED)
-    # Create a new frame
-    global second_frame
-    global uplifted_fuel_parameters_frame
-    second_frame = tkinter.Frame(frame)
-    second_frame.grid(row=0, column=0, sticky="news")
-    #next_page_button.configure(command=lambda: go_to_second_page(second_frame))
-    
-    fuel_quantity_indicator_frame = tkinter.LabelFrame(second_frame, text = "Fuel Quantity Indicator Accuracy")
-    fuel_quantity_indicator_frame.grid(row = 2, column = 0, sticky="news", padx=20, pady=10)
+#AirCraft information 
+main_frame = tkinter.Frame(frame)
+main_frame.grid(row = 0, column = 0, padx=20, pady=10)
 
-    desnsity_temperature_frame = tkinter.LabelFrame(fuel_quantity_indicator_frame, text = "Fuel Density and Temperature")
-    desnsity_temperature_frame.grid(row = 0, column = 0, sticky="news", padx=20, pady=10)
+# Load the logo image
+logo_path = r"C:\Users\Momo\Desktop\logo1.png"  # Modify this with the actual logo file name and extension
+logo = Image.open(logo_path)
+logo = logo.resize((100,100))
+logo = ImageTk.PhotoImage(logo, size=(frame.winfo_width(), frame.winfo_height()))  # Adjust the size
 
-    Fuel_Density_label = tkinter.Label(desnsity_temperature_frame, text="Fuel Density")
-    Fuel_Density_label.grid(row=0, column=0)
-    Fuel_Density_entry = tkinter.Entry(desnsity_temperature_frame)
-    Fuel_Density_entry.grid(row=0, column=1)
+#this is the 1st label frame will be used to display the logo
+title_frame = tkinter.LabelFrame(main_frame)
+title_frame.grid(row = 0, column = 0, sticky="news", padx=20)
 
-    Fuel_Temperature_label = tkinter.Label(desnsity_temperature_frame, text="Fuel Temperature")
-    Fuel_Temperature_label.grid(row=0, column=2)
-    Fuel_Temperature_entry = tkinter.Entry(desnsity_temperature_frame)
-    Fuel_Temperature_entry.grid(row=0, column=3)
+# Create a label to display the logo
+logo_label = tkinter.Label(title_frame, image=logo)
+logo_label.grid(row=0, column=0)
 
-    Ambient_Temperature_label = tkinter.Label(desnsity_temperature_frame, text="Ambient Temperature")
-    Ambient_Temperature_label.grid(row=0, column=4)
-    Ambient_Temperature_entry = tkinter.Entry(desnsity_temperature_frame)
-    Ambient_Temperature_entry.grid(row=0, column=5)
+# Configure the column and row weights so they expand proportionally
+title_frame.grid_rowconfigure(0, weight=1)
+title_frame.grid_columnconfigure(0, weight=1)
 
-    #####################################################################
-
-    pitch_and_roll_frame = tkinter.LabelFrame(fuel_quantity_indicator_frame, text = "Air Craft Orientation ")
-    pitch_and_roll_frame.grid(row = 1, column = 0, sticky="news", padx=20, pady=10)
-
-    Pitch_label = tkinter.Label(pitch_and_roll_frame, text="Pitch")
-    Pitch_label.grid(row=0, column=0)
-    Pitch_entry = tkinter.Entry(pitch_and_roll_frame)
-    Pitch_entry.grid(row=0, column=1)
-
-    Roll_label = tkinter.Label(pitch_and_roll_frame, text="Roll")
-    Roll_label.grid(row=0, column=2)
-    Roll_entry = tkinter.Entry(pitch_and_roll_frame)
-    Roll_entry.grid(row=0, column=3)
-
-    #####################################################################
-
-    uplifted_fuel_parameters_frame = tkinter.LabelFrame(fuel_quantity_indicator_frame, text = "Uplifted Fuel Parameters - Kg")
-    uplifted_fuel_parameters_frame.grid(row = 2, column = 0, sticky="news", padx=20, pady=10)
-
-    step1_frame = tkinter.LabelFrame(uplifted_fuel_parameters_frame, text = "Step1")
-    step1_frame.grid(row = 0, column = 0, sticky="news", padx=20, pady=10)
-    stp1_volume_label = tkinter.Label(step1_frame, text="volume")
-    stp1_volume_label.grid(row=0, column=0)
-    stp1_volume_entry = tkinter.Entry(step1_frame)
-    stp1_volume_entry.grid(row=0, column=1)
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+####the title of the 2nd label frame 
+user_info_frame = tkinter.LabelFrame(main_frame, text = "user information")
+user_info_frame.grid(row = 1, column = 0, padx=20, pady=10)
 
 
-    
+##### labels found inside user_info_frame
 
-    stp1_FOB_label = tkinter.Label(step1_frame, text="FOB")
-    stp1_FOB_label.grid(row=1, column=0)
-    stp1_FOB_entry = tkinter.Entry(step1_frame)
-    stp1_FOB_entry.grid(row=1, column=1)
+#MSN label and entry field
+ac_msn_label = tkinter.Label(user_info_frame, text="MSN")
+ac_msn_label.grid(row=0, column=0)
+ac_msn_entry = tkinter.Entry(user_info_frame)
+ac_msn_entry.grid(row=1, column=0)
 
-    indicator_step1_frame = tkinter.LabelFrame(uplifted_fuel_parameters_frame, text = "step 1-Indicator")
-    indicator_step1_frame.grid(row = 0, column = 1, sticky="news", padx=20, pady=10)
-    
-    stp1_left_label = tkinter.Label(indicator_step1_frame, text="LFT")
-    stp1_left_label.grid(row=0, column=0)
-    stp1_left_entry = tkinter.Entry(indicator_step1_frame)
-    stp1_left_entry.grid(row=1, column=0)
+#aircraft version label and entry field
+ac_version_label = tkinter.Label(user_info_frame, text="Version")
+ac_version_label.grid(row=0, column=1)
+ac_version_entry= tkinter.Entry(user_info_frame)
+ac_version_entry.grid(row =1, column=1)
 
-    
-    stp1_center_label = tkinter.Label(indicator_step1_frame, text="CTR")
-    stp1_center_label.grid(row=0, column=1)
-    stp1_center_entry = tkinter.Entry(indicator_step1_frame)
-    stp1_center_entry.grid(row=1, column=1)
+#fqi part number and entry field
+ac_fqi_part_label = tkinter.Label(user_info_frame, text="FQI P/N")
+ac_fqi_part_label.grid(row=0, column=2)
+ac_fqi_part_entry = tkinter.Entry(user_info_frame)
+ac_fqi_part_entry.grid(row=1, column=2)
 
-    stp1_right_label = tkinter.Label(indicator_step1_frame, text="RT")
-    stp1_right_label.grid(row=0, column=2)
-    stp1_right_entry = tkinter.Entry(indicator_step1_frame)
-    stp1_right_entry.grid(row=1, column=2)
+#fqi serial number label and entry field
+ac_fqi_serial_label = tkinter.Label(user_info_frame, text="FQI S/N")
+ac_fqi_serial_label.grid(row=2, column=0)
+ac_fqi_serial_entry = tkinter.Entry(user_info_frame)
+ac_fqi_serial_entry.grid(row=3, column=0)
 
-    
-    #####################################################
-    step2_frame = tkinter.LabelFrame(uplifted_fuel_parameters_frame, text = "Step2")
-    step2_frame.grid(row = 1, column = 0, sticky="news", padx=20, pady=10)
-    stp2_volume_label = tkinter.Label(step2_frame, text="volume")
-    stp2_volume_label.grid(row=0, column=0)
-    stp2_volume_entry = tkinter.Entry(step2_frame)
-    stp2_volume_entry.grid(row=0, column=1)
+#stamp label and entry field
+ac_stamp_label = tkinter.Label(user_info_frame, text="Stamp")
+ac_stamp_label.grid(row=2, column=1)
+ac_stamp_entry= tkinter.Entry(user_info_frame)
+ac_stamp_entry.grid(row =3, column=1)
 
-    stp2_FOB_label = tkinter.Label(step2_frame, text="FOB")
-    stp2_FOB_label.grid(row=1, column=0)
-    stp2_FOB_entry = tkinter.Entry(step2_frame)
-    stp2_FOB_entry.grid(row=1, column=1)
+#date label and entry field
+today= date.today()
 
-    indicator_step2_frame = tkinter.LabelFrame(uplifted_fuel_parameters_frame, text = "step 2-Indicator")
-    indicator_step2_frame.grid(row = 1, column = 1, sticky="news", padx=20, pady=10)
-    
-    stp2_left_label = tkinter.Label(indicator_step2_frame, text="LFT")
-    stp2_left_label.grid(row=0, column=0)
-    stp2_left_entry = tkinter.Entry(indicator_step2_frame)
-    stp2_left_entry.grid(row=1, column=0)
+date_label = tkinter.Label(user_info_frame, text="Date")
+date_label.grid(row=2, column=2)
+date_entry = DateEntry(user_info_frame, date_pattern='dd/mm/yyyy', mindate=today, state="readonly")
+date_entry.grid(row=3, column=2, padx=5, pady=5)
 
-    
-    stp2_center_label = tkinter.Label(indicator_step2_frame, text="CTR")
-    stp2_center_label.grid(row=0, column=1)
-    stp2_center_entry = tkinter.Entry(indicator_step2_frame)
-    stp2_center_entry.grid(row=1, column=1)
+###########the title of the 3rd label frame 
+engine_info_frame = tkinter.LabelFrame(main_frame, text = "Engine information")
+engine_info_frame.grid(row = 2, column = 0, sticky="news", padx=20, pady=10)
 
-    stp2_right_label = tkinter.Label(indicator_step2_frame, text="RT")
-    stp2_right_label.grid(row=0, column=2)
-    stp2_right_entry = tkinter.Entry(indicator_step2_frame)
-    stp2_right_entry.grid(row=1, column=2)
+##########Engine type label and entry field
+ac_engine_label = tkinter.Label(engine_info_frame, text="Engine Type")
+ac_engine_label.grid(row=0, column=0)
+ac_engine_combobox = ttk.Combobox(engine_info_frame, values=["CFM", "PW1100G", "CFM"], state="readonly")
+ac_engine_combobox.grid(row=1, column=0)
 
-    
-    ######################################################
-    step3_frame = tkinter.LabelFrame(uplifted_fuel_parameters_frame, text = "Step3")
-    step3_frame.grid(row = 2, column = 0, sticky="news", padx=20, pady=10)
-    stp3_volume_label = tkinter.Label(step3_frame, text="volume")
-    stp3_volume_label.grid(row=0, column=0)
-    stp3_volume_entry = tkinter.Entry(step3_frame)
-    stp3_volume_entry.grid(row=0, column=1)
+########## Create two check buttons, link them to the IntVar, and set their values
 
-    stp3_FOB_label = tkinter.Label(step3_frame, text="FOB")
-    stp3_FOB_label.grid(row=1, column=0)
-    stp3_FOB_entry = tkinter.Entry(step3_frame)
-    stp3_FOB_entry.grid(row=1, column=1)
+checkbutton1 = tkinter.Radiobutton(engine_info_frame, text="KILO", variable=ki_lo_var, value=1)
+checkbutton1.grid(row=0, column=1)
+checkbutton2 = tkinter.Radiobutton(engine_info_frame, text="LIVRE", variable=ki_lo_var, value=2)
+checkbutton2.grid(row=0, column=2)
 
-    indicator_step3_frame = tkinter.LabelFrame(uplifted_fuel_parameters_frame, text = "step 3-Indicator")
-    indicator_step3_frame.grid(row = 2, column = 1, sticky="news", padx=20, pady=10)
-    
-    stp3_left_label = tkinter.Label(indicator_step3_frame, text="LFT")
-    stp3_left_label.grid(row=0, column=0)
-    stp3_left_entry = tkinter.Entry(indicator_step3_frame)
-    stp3_left_entry.grid(row=1, column=0)
 
-    
-    stp3_center_label = tkinter.Label(indicator_step3_frame, text="CTR")
-    stp3_center_label.grid(row=0, column=1)
-    stp3_center_entry = tkinter.Entry(indicator_step3_frame)
-    stp3_center_entry.grid(row=1, column=1)
 
-    stp3_right_label = tkinter.Label(indicator_step3_frame, text="RT")
-    stp3_right_label.grid(row=0, column=2)
-    stp3_right_entry = tkinter.Entry(indicator_step3_frame)
-    stp3_right_entry.grid(row=1, column=2)
-
-    back_close_frame = tkinter.Frame(second_frame)
-    back_close_frame.grid(row = 6, column = 0, sticky="news", padx=20, pady=20)
-    #Back Button    
-    back_button = tkinter.Button(back_close_frame, text="Back", command=lambda: show_second_frame(second_frame))
-    back_button.grid(row=6, column=0, pady=(10, 0))
-
-    error_label = tkinter.Label(second_frame, text="", fg="red")
-    error_label.grid(row=7, column=0, pady=(0, 10))
-
-    
-    def on_run(): 
-        if checker_page2 == 1:
-            step1_uplifted = float(stp1_volume_entry.get()) * (float(Fuel_Density_entry.get()) + (float(Ambient_Temperature_entry.get()) - float(Fuel_Temperature_entry.get()))*0.0008)
-            step2_uplifted = float(stp2_volume_entry.get()) * (float(Fuel_Density_entry.get()) + (float(Ambient_Temperature_entry.get()) - float(Fuel_Temperature_entry.get()))*0.0008)
-            step3_uplifted = float(stp3_volume_entry.get()) * (float(Fuel_Density_entry.get()) + (float(Ambient_Temperature_entry.get()) - float(Fuel_Temperature_entry.get()))*0.0008)
-        elif checker_page2 == 2:
-            step1_uplifted = float(stp1_volume_entry.get()) * (float(Fuel_Density_entry.get()) + (float(Ambient_Temperature_entry.get()) - float(Fuel_Temperature_entry.get()))*0.0008) / 0.45359
-            step2_uplifted = float(stp2_volume_entry.get()) * (float(Fuel_Density_entry.get()) + (float(Ambient_Temperature_entry.get()) - float(Fuel_Temperature_entry.get()))*0.0008) / 0.45359
-            step3_uplifted = float(stp3_volume_entry.get()) * (float(Fuel_Density_entry.get()) + (float(Ambient_Temperature_entry.get()) - float(Fuel_Temperature_entry.get()))*0.0008) / 0.45359
-        
-        FOB_difference = float(stp3_FOB_entry.get()) - float(stp2_FOB_entry.get())
-        uplifted_difference = step3_uplifted - step2_uplifted
-        accuracy = (FOB_difference / uplifted_difference) * 100 - 100
-
-        page2_entry_manager = classes_page2.page2_EntryManager(Fuel_Density_entry, Fuel_Temperature_entry, Ambient_Temperature_entry, Pitch_entry, Roll_entry,
-                 stp1_volume_entry, stp1_FOB_entry, step1_uplifted, stp1_left_entry, stp1_center_entry, stp1_right_entry,
-                 stp2_volume_entry, stp2_FOB_entry, step2_uplifted, stp2_left_entry, stp2_center_entry, stp2_right_entry,
-                 stp3_volume_entry, stp3_FOB_entry, step3_uplifted, stp3_left_entry, stp3_center_entry, stp3_right_entry)
-        
-        if page2_entry_manager.are_entries_filled():
-            dictionary_page2 = page2_entry_manager.store_entries()
-            with open('dictionary_page0.pkl', 'rb') as file:
-                dictionary_page0 = pickle.load(file)
-
-            # Merge dictionaries
-            dictionary_page0.update(dictionary_page2)
-            with open('dictionary_page0.pkl', 'wb') as file:
-                pickle.dump(dictionary_page0, file)
-            main()
-            print(step1_uplifted)
-            print(step2_uplifted)
-            print(step3_uplifted)
-            print(f'{accuracy:.2f}%')
-            #go_to_second_page()
-            error_label.config(text="")  # Clear any previous error message
+entry_manager = classes.EntryManager(ac_msn_entry, ac_version_entry, ac_fqi_part_entry, ac_fqi_serial_entry, ac_stamp_entry, date_entry, ac_engine_combobox, ki_lo_var)
+frame_manager.created_frames.append(main_frame)
+def on_start():
+    if entry_manager.are_entries_filled():
+        dictionary_page0 = entry_manager.handle_store_entries()
+        with open('dictionary_page0.pkl', 'wb') as file:
+            pickle.dump(dictionary_page0, file)
+        create_new_frame(frame, next_page_button, start_button)
+        configure_text_kilo_or_livre(ki_lo_var)
+        error_label.config(text="")  # Clear any previous error message
         # hide_page1_frames()
         # entry_manager.reset_entries()
-        else:
-            error_label.config(text="Error: Please fill in all the entries.")
-            tkinter.messagebox.showerror("Error", "Please fill in all the entries.")
+    else:
+        error_label.config(text="Error: Please fill in all the entries.")
+        tkinter.messagebox.showerror("Error", "Please fill in all the entries.")
+###########the title of the 4th label frame 
+start_reset_frame = tkinter.Frame(main_frame)
+start_reset_frame.grid(row = 3, column = 0, sticky="news", padx=20, pady=20)
 
-    run_button = tkinter.Button(back_close_frame, text="run", command=on_run)
-    run_button.grid(row=6, column=1, pady=(10, 0))
+start_button = tkinter.Button(start_reset_frame, text="Start", command = on_start)
+start_button.grid(row=0, column=0)
+error_label = tkinter.Label(main_frame, text="", fg="red")
+error_label.grid(row=4, column=0, pady=(0, 10))
 
-    def show_second_frame(frame2tobedestroyed):
+
+def on_reset(): 
+    entry_manager.reset_entries()
+    #erase_page1_entries()
+
+def on_next_page_button():
+    if entry_manager.are_entries_filled():
+        entry_manager.handle_store_entries()
+        dictionary_page0 = entry_manager.handle_store_entries()
+        with open('dictionary_page0.pkl', 'wb') as file:
+            pickle.dump(dictionary_page0, file)
+        configure_text_kilo_or_livre(ki_lo_var)
+        go_to_next_page()
+        error_label.config(text="")  # Clear any previous error message
+        # hide_page1_frames()
+        # entry_manager.reset_entries()
+    else:
+
+        error_label.config(text="Error: Please fill in all the entries.")
+        tkinter.messagebox.showerror("Error", "Please fill in all the entries.")
+
+
     
-    # Destroy the new frame
-        frame2tobedestroyed.grid_forget()
+reset_button = tkinter.Button(start_reset_frame, text="Reset", command= on_reset)
+reset_button.grid(row=0, column=2, padx=5)
+
+next_page_button = tkinter.Button(start_reset_frame, text="Next Page", command=on_next_page_button)
+next_page_button.grid(row=0, column=1)
+next_page_button.configure(state=tkinter.DISABLED)  # Initially disabled
+
+
+window.mainloop()
